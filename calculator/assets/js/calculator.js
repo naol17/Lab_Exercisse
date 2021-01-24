f27a3b560db8e4b903e41a5c54fec435da9f44a8
Naol Dame <naododa5566@gmail.com>
2021-01-24 20:01:42 +0300

    First commit creating the calculator functions.

---
+++ calculator/assets/js/calculator.js
@@ -0,0 +1,80 @@
+//Simple Calculator!!!
+
+opratercontainer = ['1: Addition\n', '2: Subtraction\n', '3: multiplication\n', '4: Division\n'];
+
+let entry;
+entry = window.prompt(opratercontainer);
+
+
+switch (entry) {
+    case "1":
+        addition();
+        break;
+    case "2":
+        Subtraction();
+        break;
+    case "3":
+        multiplication();
+        break;
+    case "4":
+        division();
+        break;
+    default:
+        window.prompt("Error Out of range!!.")
+        break;
+
+}
+
+function addition() {
+    let sum = 0
+    let intake = new Array();
+    let amount = prompt("enter amount of nums to add")
+    for (let i = 0; i < parseInt(amount); i++) {
+        intake[i] = prompt("enter the numbers" + (i + 1));
+    }
+    intake.forEach(function(member) {
+        sum = sum + parseInt(member);
+
+    })
+    window.alert("The answer of your number is: " + sum);
+
+}
+
+function Subtraction() {
+    let x = prompt("first num");
+    let y = prompt("second num");
+    w = parseInt(x);
+    v = parseInt(y);
+    var z = w - v;
+    window.alert(z);
+}
+
+function multiplication() {
+    let sum = 1
+    let intake = new Array();
+    let amount = prompt("enter amount of nums to Mutiply")
+    for (let i = 0; i < parseInt(amount); i++) {
+        intake[i] = prompt("enter the numbers" + (i + 1));
+    }
+    intake.forEach(function(member) {
+        sum = sum * parseInt(member);
+
+    })
+    window.alert("The answer of your number is: " + sum);
+
+}
+
+function division() {
+    let x = prompt("first num");
+    let y = prompt("second num");
+    w = parseInt(x);
+    v = parseInt(y);
+
+    if (v < 1) {
+        window.alert("pls enter num greater than 0 for dinomnetor")
+    } else {
+        var z = w / v;
+        window.alert("Your answer is: " + z);
+    }
+
+}
