043afcdd4407a3d7363d934e5589d3432c466121
Naol Dame <naododa5566@gmail.com>
2021-01-24 21:39:38 +0300

    change is made on transfer and withdrow methods.

--- BankSystem/asset/js/bank.js
+++ BankSystem/asset/js/bank.js
@@ -1,27 +1,29 @@
 //Bank System!!! 
 
+
+
 var initialamount = 1000;
 
 function deposit() {
-    let amount = prompt("enter amount of Deposit to be made.")
+    let amount = prompt("Enter amount of Deposit to be made.")
     afterdepo = initialamount + parseInt(amount)
     initialamount = afterdepo;
-    window.alert("Deposit made and your balance is: " + afterdepo);
+    window.alert("Deposit is made and your balance is: " + afterdepo);
 
 }
 
 function withdraw() {
-    let wzdrawal
-    let draw = window.prompt("amt wz draw")
+    let draw = window.prompt("Enter Amount to Windrow")
     let x = parseInt(draw);
 
-    if (x > initialamount) {
+    if (x > initialamount || x < 100) {
         window.alert("Your balance is insufficient for this service");
 
     } else {
+        var wzdrawal;
 
         wzdrawal = initialamount - x;
-        window.alert("wzdraw made & ur remeining balance is : " + wzdrawal);
+        window.alert("windrow made & your remaining balance is : " + wzdrawal);
 
     }
 
@@ -45,50 +47,50 @@ function transfer(x, y) {
     x = prompt("Enter Receiver name");
     y = prompt("Enter Account number");
     let transaction;
-    let amounttransfer = prompt("Enter amnt")
+    let amounttransfer = prompt("Enter amount")
 
     transmoney = parseInt(amounttransfer);
 
     if (x != trans.personname || y != trans.accnum) {
-        window.alert("incorrect user or accnum")
+        window.alert("Incorrect user Name or account number!..")
     } else {
         if (transmoney > initialamount && transmoney < 100) {
-            window.alert("Your baance is insuficent ")
+            window.alert("Your balance is insufficient ")
         } else {
             transaction = initialamount - transmoney;
-            window.alert("Trans made to : " + x + " : " + " accnum " + y + " and  ur remaining balance is: " + transaction)
+            window.alert("Trans made to : " + x + " : " + " And account number: " + y + " and  ur remaining balance is: " + transaction)
         }
     }
 
 }
 
-
-do {
-    window.alert("WelCome what Action Do yur prefer?")
-    choice = ['1: Deposit\n', '2: withdraw\n', '3: Check Balance\n', '4: Transfer\n', "0 to exit"];
-    let entry;
-    entry = window.prompt(choice);
+window.alert("WelCome what Action Do yur prefer?")
+choice = ['1: Deposit\n', '2: withdraw\n', '3: Check Balance\n', '4: Transfer\n', "0 to exit"];
+let entry;
+entry = window.prompt(choice);
 
 
+switch (entry) {
+    case "1":
+        deposit();
+        break;
+    case "2":
+        withdraw();
+        break;
+    case "3":
+        balance();
+        break;
+    case "4":
+        transfer();
+        break;
+    default:
+        window.prompt("Error Service not found!!.")
+        break;
 
-
-    switch (entry) {
-        case "1":
-            deposit();
-            break;
-        case "2":
-            withdraw();
-            break;
-        case "3":
-            balance();
-            break;
-        case "4":
-            transfer();
-            break;
-        default:
-            window.prompt("Error Service not found!!.")
-            break;
-
-    }
 }
-while (entry = !'0');
+
+
+
+console.log("***************************************************");
+console.log("Your Balance is: " + initialamount);
+console.log("***************************************************");
